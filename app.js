22eaa25748a7de972b0ee760b54fb77d03df112a
SalvisR <salvis.reimanis@inbox.lv>
2020-11-25 16:13:09 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+  parent: document.querySelector(".distortion"),
+  intensity: 1,
+  image1: "./1.mp4",
+  image2: "./2.mp4",
+  displacementImage: "./heightMap.png",
+  video: true
+});
